0cc7a4d629552fa5fc09265b1e1acfdf563de2be
Dana Bouchard <{dbouchard}@lifefarout.com>
2013-04-02 13:53:09 -0400

    58 examples passing

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -19,8 +19,7 @@ describe "User pages" do
   	it { should have_selector('title', text: user.name) }
   end
 
-    describe "signup" do
-
+  describe "signup" do
     before { visit signup_path }
 
     let(:submit) { "Create my account" }
@@ -56,6 +55,23 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "edit" do
+        let(:user) {FactoryGirl.create(:user) }
+        before { visit edit_user_path (user) }
+
+        describe "page" do
+          it { should have_selector('h1',     text: "Update your profile") }
+          it { should have_selector('title',  text: "Edit user") }
+          it { should have_link('change', href: 'http://gravatar.com/emails') }
+        end
+
+        describe "with invalid information" do
+          before { click_button "Save changes" }
+
+          it { should have_content('error') } 
+        end
       end
     end
   end
