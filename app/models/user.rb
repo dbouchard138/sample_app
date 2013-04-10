c9d35b134bdabde6508303d5d242cc9b04024c15
Dana Bouchard <dbouchard@lifefarout.com>
2013-04-09 22:25:40 -0400

    (154 test/2failing) in user_spec.rb, ready to start 11.3.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,6 +38,10 @@ def unfollow!(other_user)
   relationships.find_by_followed_id(other_user.id).destroy
 end
 
+def feed
+  Micropost.from_users_followed_by(self)
+end
+
   private
 
     def create_remember_token
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,3 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
 
 require 'spec_helper'
 
@@ -163,24 +153,7 @@ describe User do
         Micropost.find_by_id(micropost.id).should be_nil
       end
     end
-
-    describe "microposts associations" do 
-
-      before { @user.save }
-      let!(:older_micropost) do
-        FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
-      end 
-
-      describe "status" do
-        let(:unfollowed_post) do 
-        end
-
-        its(:feed) { should include(newer_micropost) }          
-        its(:feed) { should include(older_micropost) }
-        its(:feed) { should_not include(unfollowed_post) }
-      end
-    end
-  end 
+  end
 
   describe "following" do
     let(:other_user) { FactoryGirl.create(:user) }
@@ -206,8 +179,30 @@ describe User do
       subject { other_user }
       its(:followers) { should include(@user) }
     end
+
+    describe "status" do
+      let(:unfollowed_post) do
+        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+      let(:followed_user) { FactoryGirl.create(:user) }
+
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
+      end
+
+      its(:feed) { should include(newer_micropost) }
+      its(:feed) { should include(older_micropost) }
+      its(:feed) { should_not include(unfollowed_post) }
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+        end
+      end
+    end
   end
 end
+
 
 
 
