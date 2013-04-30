89352bec576f7790333ee55483957062be768ce7
Dana Bouchard <dbouchard@lifefarout.com>
2013-04-30 16:15:38 -0400

    Created spec folders to define custom matchers and macros, edited spec_helper to take deffinitions

---
+++ spec/controller_macros.rb
@@ -0,0 +1,9 @@
+module ControllerMacros
+end
+
+
+
+#                      Dana Say's
+# Here i can define custom matchers and use them everywhere 
+# throughout my application instead of defining them with in 
+# the same descibe block every time.
---
+++ spec/custom_matcher.rb
@@ -0,0 +1,12 @@
+module CustomMatchers
+	def be_one_more_than(number)
+		simple_matcher("one more than #{numer}") { |actual| actual == number=1 }
+	end
+end
+
+
+
+               # Dana Say's
+# Here i can define custom matchers and use them everywhere 
+# throughout my application instead of defining them with in 
+# the same descibe block every time.
